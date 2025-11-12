79d65d98af626356dc34a29d862fbf36d1e524e0
sbeeredd04 <sbeeredd@asu.edu>
2025-11-12 05:15:25 -0700

    fix: Remove hardcoded hf_url from ImageFolderWrapper model configurations

--- token-opt/tto/imagefolder_wrapper.py
+++ token-opt/tto/imagefolder_wrapper.py
@@ -34,7 +34,6 @@ class ImageFolderWrapper(nn.Module):
             'codebook_drop': 0.1,
             'half_sem': True,
             'start_drop': 3,
-            'hf_url': 'https://huggingface.co/qiuk6/XQ-GAN/resolve/main/MSVR10P2-4096/best_ckpt.pt',
         },
         'MSVR10P2-8192': {
             'codebook_size': 8192,
@@ -52,7 +51,6 @@ class ImageFolderWrapper(nn.Module):
             'codebook_drop': 0.1,
             'half_sem': True,
             'start_drop': 3,
-            'hf_url': 'https://huggingface.co/qiuk6/XQ-GAN/resolve/main/MSVR10P2-8192/best_ckpt.pt',
         },
         'MSVR10P2-16384': {
             'codebook_size': 16384,
@@ -70,7 +68,6 @@ class ImageFolderWrapper(nn.Module):
             'codebook_drop': 0.1,
             'half_sem': True,
             'start_drop': 3,
-            'hf_url': 'https://huggingface.co/qiuk6/XQ-GAN/resolve/main/MSVR10P2-16384/best_ckpt.pt',
         },
     }
     
